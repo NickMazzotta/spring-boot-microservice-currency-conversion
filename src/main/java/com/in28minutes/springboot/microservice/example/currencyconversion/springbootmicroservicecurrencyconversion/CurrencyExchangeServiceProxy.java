a36aef73cf9d6e34dc3f5a017c9c1d62dc4dfbd5
NickMazzotta <nickamazzotta@gmail.com>
2018-07-18 19:14:26 -0400

    added comments

--- src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/currencyconversion/springbootmicroservicecurrencyconversion/CurrencyExchangeServiceProxy.java
@@ -4,6 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
+// hard coded connection to one instance of forex service
 @FeignClient(name="forex-service", url="localhost:8000")
 public interface CurrencyExchangeServiceProxy {
 
